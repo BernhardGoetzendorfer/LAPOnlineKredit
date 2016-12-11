7561ae0addc4a6ff749ed621a9e9934aca6113fc
Kanevry <bernhardgoetzendorfer95@gmail.com>
2016-12-11 16:09:58 +0100

    added ArbeitgeberModel

--- LAPOnlineKredit.web/Models/ArbeitgeberModel.cs
+++ LAPOnlineKredit.web/Models/ArbeitgeberModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,31 @@ namespace LAPOnlineKredit.web.Models
 {
     public class ArbeitgeberModel
     {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Pflichtfeld")]
+        [StringLength(30, ErrorMessage = "Maximal 30 Zeichen")]
+        [Display(Name = "Ihr Firmenname")]
+        public string FirmenName { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Pflichtfeld")]
+        [Display(Name = "Ihre Beschäftigungsart")]
+        public int ID_BeschäftigungsArt { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Pflichtfeld")]
+        [Display(Name = "Ihre Branche")]
+        public int ID_Branche { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, ConvertEmptyStringToNull = true, DataFormatString = "MM.yyyy")]
+        [Display(Name = "Seit wann sind Sie beschäftigt? (MM.YYYY)")]
+        public string BeschäftigtSeit { get; set; }
+
+        public List<BeschaeftigungsArtModel> AlleBeschaeftigungen { get; set; }
+        public List<BrancheModel> AlleBranchen { get; set; }
+
+        public int ID_Kunde { get; set; }
+
+
+
+
     }
 }
